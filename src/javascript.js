56e6ab0559069e450b678c9e2476c55abd21ba55
ToxA88 <40351855+ToxA88@users.noreply.github.com>
2024-09-20 20:52:28 +0300

    firt_commit

--- src/javascript.js
+++ src/javascript.js
@@ -1,1 +1,14 @@
-console.log("hello from javascript")
+console.log ("hello from javascript");
+// alert("Привет")
+// confirm("Узнали?")
+// let ans;
+// ans = prompt("Укажите код операции :");
+// if (ans == "6453"){
+//     alert("Молодец!");
+// }
+// else{
+//     location.reload()
+// }   
+let x;
+x = prompt("Введите число:")
+Number(x);
