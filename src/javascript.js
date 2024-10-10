ded58bba9e16ff15a2bc8d84ab692938e37d3d9d
ToxA88 <40351855+ToxA88@users.noreply.github.com>
2024-10-10 18:32:16 +0300

    first_commit

--- src/javascript.js
+++ src/javascript.js
@@ -76,79 +76,79 @@ console.log ("hello from javascript");
 // u = Number(u);
 // alert((parseInt(u) % 2 == 0) ? "Ваше число чётное" : "Ваше число нечётное");
 
-var obj = new Object()
-var obl2 = {}
-var student = new Object();
-student["Name"] = ("Anton");
-student["Age"] = "23 года";
-console.log(student["Name"]);
-console.log(student["Age"]);
-var firm = {}
-firm.Name = "Star Inc";
-firm.Address = "Nevskiy prospect";
-console.log(firm.Name);
-console.log(firm.Address);
-var dog = {}
-dog["Name of dog"] = "Caesar";
-dog.Age = "3 года";
-console.log(dog["Name of dog"]);
-console.log(dog.Age);
-var cat = {}
-cat.Name = "Pusya";
-cat.Age = "2 года";
-console.log(cat.Name);
-console.log(cat.Age);
-if ("Address" in firm) {
-  console.log("da " + firm.Address);
-}
-else {
-  console.log("netu");
-}
+// var obj = new Object()
+// var obl2 = {}
+// var student = new Object();
+// student["Name"] = ("Anton");
+// student["Age"] = "23 года";
+// console.log(student["Name"]);
+// console.log(student["Age"]);
+// var firm = {}
+// firm.Name = "Star Inc";
+// firm.Address = "Nevskiy prospect";
+// console.log(firm.Name);
+// console.log(firm.Address);
+// var dog = {}
+// dog["Name of dog"] = "Caesar";
+// dog.Age = "3 года";
+// console.log(dog["Name of dog"]);
+// console.log(dog.Age);
+// var cat = {}
+// cat.Name = "Pusya";
+// cat.Age = "2 года";
+// console.log(cat.Name);
+// console.log(cat.Age);
+// if ("Address" in firm) {
+//   console.log("da " + firm.Address);
+// }
+// else {
+//   console.log("netu");
+// }
 
-var student2 = {
-  Name: "Vitas",
-  LastName: "Petrov",
-  age: "23 года",
-}
-console.log(student2)
-console.log(student2.Name)
-console.log(student2.LastName)
-console.log(student2.age)
+// var student2 = {
+//   Name: "Vitas",
+//   LastName: "Petrov",
+//   age: "23 года",
+// }
+// console.log(student2)
+// console.log(student2.Name)
+// console.log(student2.LastName)
+// console.log(student2.age)
 
-const message = new String("Hello");
-console.log(message[3]);
-console.log(message[0]);
-console.log(message[2]);
-console.log(message.length,"simvolov");
-console.log(message.repeat(2))
+// const message = new String("Hello");
+// console.log(message[3]);
+// console.log(message[0]);
+// console.log(message[2]);
+// console.log(message.length,"simvolov");
+// console.log(message.repeat(2))
 
-const hello = "привет мир . пока мир .";
-const key = "мир".padStart(10,"привет ");
-const world = hello.substring(6,0);
-const bey = hello.substring(13,17);
-const bye = hello.slice(-10,-1);
-const firstPos = hello.indexOf(key);
-const lastPos = hello.lastIndexOf(key);
-const eye = hello.substr(6,12);
-const bor = hello.concat(key);
-const tir = bye.replace("пока","привет");
-const heyParts = hello.split(" ");
-console.log(firstPos);
-console.log(lastPos);
-console.log(hello.includes("пока",12));
-console.log(hello.includes("мир",18));
-console.log(world);
-console.log(bey);
-console.log(bye);
-console.log(eye);
-console.log(eye.toUpperCase());
-console.log(eye.charCodeAt(1),eye.charAt(1));
-console.log(eye.trim());
-console.log(bor)
-console.log(tir.repeat(3))
-console.log(heyParts)
-console.log(hello.startsWith("п"))
-console.log(hello.endsWith("."))
-console.log(hello.startsWith("р",9))
-console.log(hello.startsWith("п",hello.length-10))
-console.log(key)
+// const hello = "привет мир . пока мир .";
+// const key = "мир".padStart(10,"привет ");
+// const world = hello.substring(6,0);
+// const bey = hello.substring(13,17);
+// const bye = hello.slice(-10,-1);
+// const firstPos = hello.indexOf(key);
+// const lastPos = hello.lastIndexOf(key);
+// const eye = hello.substr(6,12);
+// const bor = hello.concat(key);
+// const tir = bye.replace("пока","привет");
+// const heyParts = hello.split(" ");
+// console.log(firstPos);
+// console.log(lastPos);
+// console.log(hello.includes("пока",12));
+// console.log(hello.includes("мир",18));
+// console.log(world);
+// console.log(bey);
+// console.log(bye);
+// console.log(eye);
+// console.log(eye.toUpperCase());
+// console.log(eye.charCodeAt(1),eye.charAt(1));
+// console.log(eye.trim());
+// console.log(bor)
+// console.log(tir.repeat(3))
+// console.log(heyParts)
+// console.log(hello.startsWith("п"))
+// console.log(hello.endsWith("."))
+// console.log(hello.startsWith("р",9))
+// console.log(hello.startsWith("п",hello.length-10))
+// console.log(key)
